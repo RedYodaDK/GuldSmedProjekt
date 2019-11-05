999bc9da7fa5b07510cf9623bbb2590e893a929b
Jacob Schrøder Ipsen <zbcjaips1@zbc.dk>
2019-11-05 15:22:53 +0100

    Dyn

--- Dynamisk.cs
+++ Dynamisk.cs
@@ -8,5 +8,43 @@ namespace GuldSmed
 {
     class Dynamisk
     {
+        public static int GetPriceRight(int length)
+        {
+            int before = 0;
+            int temp = 0;
+
+            for (int i = 1; i < length; i++)
+            {
+                if (i < 30)
+                {
+                    if (i < 1)
+                    {
+                        temp = 1;
+                    }
+                    else
+                    {
+                        temp = 140;
+                    }
+                }
+                else
+                {
+                    temp = 300;
+                }
+
+                before += temp;
+            }
+
+
+
+
+
+
+        }
+
+
+
+
+
+
     }
 }
