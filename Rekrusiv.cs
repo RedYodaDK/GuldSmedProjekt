c7e436bdba6dbc5d921807d7f7b6a1a5115ebb8d
RedYodaDK <stigfrederiksen32@gmail.com>
2019-11-05 15:22:18 +0100

    Dictonary to change stuff, and final rekusiv funktion

--- Rekrusiv.cs
+++ Rekrusiv.cs
@@ -8,10 +8,8 @@ namespace GuldSmed
 {
     class Rekrusiv
     {
-        public static int GetPriceRight(int length)
+        public static int GetPriceRight(int length, Dictionary<int, int> vals)
         {
-            int temp = 0;
-
             if (length == 0)
             {
                 return 0;
@@ -22,67 +20,31 @@ namespace GuldSmed
                 return int.MinValue;
             }
 
-            if (length < 13)
+            if (length == 13)
             {
-                temp = 1;
+                return 140;
             }
 
-            if (length < 30)
+            if (length == 30)
             {
-                if (temp < 140)
-                {
-                    temp = 140;
-                }
+                return 300;
             }
 
-            if (length > 30)
-            {
-                if (temp < 300)
-                {
-                    temp = 300;
-                }
-            }
+            int arm = GetPriceRight(length - 14,vals)+vals[13]+vals[1];
+            int hals = GetPriceRight(length - 31, vals) + vals[30] + vals[1];
 
-            int one = GetPriceRight(length - 1)+1;
-            int arm = GetPriceRight(length - 13)+140;
-            int hals = GetPriceRight(length - 30)+300;
-
-            if (hals > arm && hals > one)
+            if (hals >= arm && hals >= length)
             {
                 return hals;
             }
-            else if (arm > one && arm > hals)
+            else if (arm >= length && arm >= hals)
             {
                 return arm;
-            }
-            else
-            {
-                return one;
-            }
-
-            
-
-        }
-        /*
-        public static int GetBestPriceForChain(int length)
-        {
-            if (length <= 0)
-            {
-                return 0;
-            } 
-            else if (length > 30)
-            {
-                return 301 + GetBestPriceForChain(length - 31);
-            }
-            else if (length > 13)
-            {
-                return 101 + GetBestPriceForChain(length - 14);
             }
             else
             {
                 return length;
             }
         }
-        */
     }
 }
