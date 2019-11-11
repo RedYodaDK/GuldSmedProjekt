e840cde9e08f154d9633854822e5abf0559caeb1
RedYodaDK <stigfrederiksen32@gmail.com>
2019-11-11 11:19:39 +0100

    Timing

--- Program.cs
+++ Program.cs
@@ -8,6 +8,18 @@ namespace GuldSmed
 {
     class Program
     {
+        static void TimeAndRunGetPriceRight(int input)
+        {
+            var watchRekusiv = System.Diagnostics.Stopwatch.StartNew();
+            int rekusivInt = Rekrusiv.GetPriceRight(input, values);
+            watchRekusiv.Stop();
+            var watchDynamisk = System.Diagnostics.Stopwatch.StartNew();
+            int dynamiskInt = Dynamisk.GetPriceRight(input, values);
+            watchDynamisk.Stop();
+            Console.WriteLine("Bedste Pris Recursiv: " + rekusivInt + "og tog " + watchRekusiv.ElapsedMilliseconds + " Ms");
+            Console.WriteLine("Bedste Pris Dynamisk: " + dynamiskInt + "og tog " + watchDynamisk.ElapsedMilliseconds + " Ms");
+        }
+
         static Dictionary<int, int> values = new Dictionary<int, int>
         {
             {1,  1},
@@ -18,8 +30,9 @@ namespace GuldSmed
         static void Main(string[] args)
         {
             string input = Console.ReadLine();
-            Console.WriteLine("Bedste Pris Recursiv: " + Rekrusiv.GetPriceRight(int.Parse(input), values));
-            Console.WriteLine("Bedste Pris Dynamisk: " + Dynamisk.GetPriceRight(int.Parse(input), values));
+            TimeAndRunGetPriceRight(int.Parse(input));
+            /*Console.WriteLine("Bedste Pris Recursiv: " + Rekrusiv.GetPriceRight(int.Parse(input), values));
+            Console.WriteLine("Bedste Pris Dynamisk: " + Dynamisk.GetPriceRight(int.Parse(input), values));*/
             Console.ReadKey();
         }
     }
